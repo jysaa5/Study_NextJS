665a430f95828b0060284f9e25a9c4297ae33519
jysaa5 <jysaa5@naver.com>
2023-05-13 00:11:28 +0900

    :tada: next 13 버전으로 만들어보기

--- next-test-app-1/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-const Home: NextPage = () => <div>Home</div>;
-
-export default Home;
