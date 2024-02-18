d28c6baf55ce46ed60777f9dfcdf7dfbcad9bcf3
jysaa5 <jysaa5@naver.com>
2024-02-18 13:23:35 +0900

    :lipstick: p태그 추가

--- next-app-2/src/app/page.tsx
+++ next-app-2/src/app/page.tsx
@@ -6,7 +6,11 @@ export default function Home() {
       <h1 style={{ color: "white", textAlign: "center" }}>Time to get started!</h1>
       <p>
         <Link href="/meals">Meals</Link>
+      </p>
+      <p>
         <Link href="/meals/share">Share Meal</Link>
+      </p>
+      <p>
         <Link href="/community">Community</Link>
       </p>
     </main>
