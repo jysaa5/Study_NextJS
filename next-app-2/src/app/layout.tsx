3375ecddffe73a891896a4ef1c41e86bec0c8bb6
jysaa5 <jysaa5@naver.com>
2024-02-22 00:02:29 +0900

    헤더 추가

--- next-app-2/src/app/layout.tsx
+++ next-app-2/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import MainHeader from "./components/main-header";
 import "./globals.css";
 
 export const metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: props) {
             <path fill="url(#gradient)" d="M0,256L48,240C96,224,192,192,288,181.3C384,171,480,181,576,186.7C672,192,768,192,864,181.3C960,171,1056,149,1152,133.3C1248,117,1344,107,1392,101.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
           </svg>
         </div>
+        <MainHeader />
         {children}
       </body>
     </html>
