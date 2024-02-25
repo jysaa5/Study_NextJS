369f1cea6b5305d59f3977edad7404f8e3cb642e
jysaa5 <jysaa5@naver.com>
2024-02-26 00:42:08 +0900

    네비게이션 스타일 추가

--- next-app-2/src/app/components/main-header.tsx
+++ next-app-2/src/app/components/main-header.tsx
@@ -3,12 +3,12 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
         <img src={logoImg.src} alt="A plate width food on it" />
         NextLevel Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
