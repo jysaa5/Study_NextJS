6653cbbe4b3ad21b6fda793bb9ca9c3aef7bb295
jysaa5 <jysaa5@naver.com>
2024-03-13 00:15:48 +0900

    next/navigation 사용

--- next-app-14-2/src/app/section1/links/page.tsx
+++ next-app-14-2/src/app/section1/links/page.tsx
@@ -1,13 +1,18 @@
+'use client'
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-const LinkPage: NextPage = async () => {
-   
+
+const LinkPage: NextPage =  () => {
+    const router = useRouter();
     return (
         <main>
             <h1>Links</h1>
+            <button onClick={() => {router.push('/section1/getStaticProps')}}>/getStaticProps</button>
+            {/* <h1>Links</h1>
             <div style={{height: '200vh'}}/>
-            <Link href="/section1/getStaticProps" style={{color: 'red'}}>/getStaticProps</Link>
+            <Link href="/section1/getStaticProps" style={{color: 'red'}}>/getStaticProps</Link> */}
         </main>
     )
 }
