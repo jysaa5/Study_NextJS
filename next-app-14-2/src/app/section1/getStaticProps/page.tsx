1cd2a3d2c7e71b88cc76c144d5746776d1b031f5
jysaa5 <jysaa5@naver.com>
2024-03-13 23:59:54 +0900

    함수 위치 변경

--- next-app-14-2/src/app/section1/getStaticProps/page.tsx
+++ next-app-14-2/src/app/section1/getStaticProps/page.tsx
@@ -3,19 +3,10 @@ import { NextPage } from 'next';
 interface Props {
     data: number
 }
+
 const Example: NextPage = async () => {
-    const data = await getData();
-    return (
-        <main>
-            <h1>getStaticProps Page</h1>
-            <p>값: {data.props.data ?? ''}</p>
-        </main>
-    )
-}
 
-export default Example;
-
-export async function getData() {
+ async function getData() {
     const delayInSeconds = 2;
     const data: number = await new Promise((reslove) => setTimeout(() => reslove(Math.random()), delayInSeconds * 1000))
 
@@ -23,3 +14,15 @@ export async function getData() {
         props: {data}
     }
 }
+    const data = await getData();
+    console.log(data)
+    return (
+        <main>
+            <h1>getStaticProps Page</h1>
+            <p>값: {data?.props?.data ?? ''}</p>
+        </main>
+    )
+}
+
+export default Example;
+
